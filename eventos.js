bb1bfd4fd7d2d271422a78067ea88c9ffb29d819
agustinarolando <agustinarolando@gmail.com>
2023-08-09 09:29:29 -0300

    agregué una alternativa

--- eventos.js
+++ eventos.js
@@ -1,3 +1,8 @@
 document.getElementById("miDiv").addEventListener("click", function() {
     alert('Hola! Soy el div');
 });
+
+// Otra forma de añadir la misma alerta
+// document.getElementsByTagName("div")[0].addEventListener("click", function() {
+//     alert("Hola! Soy el div")
+// })
