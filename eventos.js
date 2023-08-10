f2f36232a0d59e14b2907c69f6fd29cf96296b15
agustinarolando <agustinarolando@gmail.com>
2023-08-10 10:00:41 -0300

    solucion click

--- eventos.js
+++ eventos.js
@@ -1,8 +1,12 @@
-document.getElementById("miDiv").addEventListener("click", function() {
-    alert('Hola! Soy el div');
-});
+function saludarDiv() {
+    alert("Hola! Soy el div");
+}
 
+document.getElementById("miDiv").addEventListener("click", saludarDiv);
+
+document.getElementsByTagName("button")[0].addEventListener("click", function() {
+    alert('Hola!');
+    document.getElementById("miDiv").removeEventListener("click", saludarDiv)
+})
 // Otra forma de añadir la misma alerta
-// document.getElementsByTagName("div")[0].addEventListener("click", function() {
-//     alert("Hola! Soy el div")
-// })
+// document.getElementsByTagName("div")[0].addEventListener("click", saludarDiv);
